ea00079db26e76b754762663ae9187c197a9f76b
Ben Scheirman <ben@scheirman.com>
2019-07-25 09:58:53 -0500

    Initial table view UI

---
+++ TodoApp/NewItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewItemCell.swift
+//  TodoApp
+//
+//  Created by Ben Scheirman on 7/25/19.
+//  Copyright © 2019 NSScreencast. All rights reserved.
+//
+
+import UIKit
+
+class NewItemCell : UITableViewCell {
+    @IBOutlet weak var textField: UITextField!
+}
---
+++ TodoApp/TodosTableViewController.swift
@@ -0,0 +1,116 @@
+//
+//  TodosTableViewController.swift
+//  TodoApp
+//
+//  Created by Ben Scheirman on 7/25/19.
+//  Copyright © 2019 NSScreencast. All rights reserved.
+//
+
+import UIKit
+
+class TodosTableViewController: UITableViewController {
+
+    var todos: [String] = []
+
+    private weak var newItemTextField: UITextField!
+    private let newItemCellIndexPath = IndexPath(row: 0, section: 1)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        for i in 0...100 {
+            todos.append("Todo item \(i)")
+        }
+
+        tableView.tableFooterView = UIView()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return todos.count
+        } else {
+            return 1
+        }
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "newItemCell", for: indexPath) as! NewItemCell
+            newItemTextField = cell.textField
+            newItemTextField.delegate = self
+            return cell
+        }
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func didTapAdd(_ sender: Any) {
+        UIView.animate(withDuration: 0.3, animations: {
+            self.tableView.scrollToRow(at: self.newItemCellIndexPath, at: .middle, animated: false)
+        }) { _ in
+            self.newItemTextField.becomeFirstResponder()
+        }
+    }
+
+}
+
+extension TodosTableViewController : UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("end editing")
+    }
+}
